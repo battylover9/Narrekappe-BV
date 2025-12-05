517fb000fde1a1d22db316286a3578edca51923f
battylover12 <rombley2002@hotmail.com>
2025-12-05 03:59:36 +0100

    fixed user portal

--- pages/contract.js
+++ pages/contract.js
@@ -23,7 +23,7 @@ export default function ContactPage() {
             <Link href="/admin">Admin</Link>
             <Link href="/security">Security</Link>
             <Link href="/contact">Contact</Link>
-            <Link href="/user-portal">User Portal</Link>
+            <Link href="/stud-dash">User Portal</Link>
             <Link href="/admin-login">Admin Login</Link>
           </nav>
           <button className="nav-toggle" aria-label="Toggle navigation">
